89398dd7420a04b8882026ef1e02a33d34e2395a
ed-castro <bladimir.1992@hotmail.com>
2022-11-03 20:52:01 -0600

    Agregado metodos de renta,mostrar y  constructor personalizado y con parametros

--- Grupo1_Herencia/Empleados.cs
+++ Grupo1_Herencia/Empleados.cs
@@ -41,11 +41,76 @@ namespace Grupo1_Herencia
            
         }
 
+        //metodo para calcular AFP
         public double DesAfp ()
         {
             return Sueldo * 0.0725;
         }
+        //metodo para calcular Renta
 
+        public double DescRenta ()
+        {
+            // TRAMO I
+            if (Sueldo>=0.01 && Sueldo<=472)
+            {
+                return 0;
+                // TRAMO II
+            } else if (Sueldo>=472.01 && Sueldo <=895.24)
+            {
+                return ((Sueldo - 472) * 0.1) + 17.67;
+
+                //TRAMO III
+            } else if (Sueldo>=895.25 && Sueldo<=2038.10)
+            {
+                return ((Sueldo - 895.24) * 0.2) + 60;
+            }
+            // TRAMO IV
+            else  
+            {
+                return ((Sueldo - 2038.10) * 0.3) + 288.57;
+            } 
+        }
+
+        // metodo para captura de datos desde teclado
+        public void Captura ()
+        {
+            Console.Write("Ingrese el nombre del emplado: ");
+            nombre= Console.ReadLine();
+            Console.Write("Ingrese el apellido del emplado: ");
+            apellido = Console.ReadLine();
+            Console.Write("Ingrese el genero: ");
+            genero = Console.ReadLine();
+            Console.Write("Ingrese la edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el salario: ");
+            sueldo = Convert.ToDouble(Console.ReadLine());
+        }
+        // Constructor por omision 
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Nombre: " + nombre + " " + apellido);
+            Console.WriteLine("Genero: " + genero );
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Sueldo: " + sueldo);
+        }
+    
+    
+
+        public Empleados ()
+        {
+            Console.WriteLine("Bienvenidos!");
+        }
+
+        public Empleados (string nombre, string apellido, string genero, int edad, double sueldo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.genero = genero;
+            this.edad = edad;
+            this.sueldo = sueldo;
+                
+        }
        
 
     }
